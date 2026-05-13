871bdf01307126ea02c3d54c9ed32e82910f24d1
Jak <jak@example.com>
2026-05-12 21:33:45 -0500

    Remove ShowStateShare call from ImGuiConsole when game is running

--- src/dusk/imgui/ImGuiConsole.cpp
+++ src/dusk/imgui/ImGuiConsole.cpp
@@ -383,7 +383,6 @@ namespace dusk {
             m_menuTools.ShowPlayerInfo();
             m_menuTools.ShowAudioDebug();
             m_menuTools.ShowSaveEditor();
-            m_menuTools.ShowStateShare();
             m_menuTools.ShowActorSpawner();
         }
 
